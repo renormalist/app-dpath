16eb0da7f16a526c9125888fdd0b1544689a5812
Steffen Schwigon <ss5@renormalist.net>
2009-04-22 08:06:05 +0200

    - first working version: read, match, write

--- lib/App/DPath/Command/search.pm
+++ lib/App/DPath/Command/search.pm
@@ -2,29 +2,103 @@ package App::DPath::Command::search;
 
 use App::DPath -command;
 
-use strict; 
+use strict;
 use warnings;
 
+use Data::DPath 'dpath';
+
 sub opt_spec {
-    return (
-	[ "blortex|X",  "use the blortex algorithm" ],
-	[ "recheck|r",  "recheck all results"       ],
-	);
+        return (
+                [ "intype|i=s",   "input format, [yaml(default), dumper]"  ],
+                [ "outtype|o=s",  "output format, [yaml(default), dumper]" ],
+               );
 }
 
 sub validate_args {
     my ($self, $opt, $args) = @_;
-    
-    # no args allowed but options!
-    $self->usage_error("No args allowed") if @$args;
+}
+
+sub read_in {
+        my ($self, $opt, $args, $file) = @_;
+
+        my $intype  = $opt->{intype}  || 'yaml';
+        print STDERR "intype: $intype\n";
+        my $data;
+        my $filecontent;
+        {
+                local $/;
+                if ($file eq '-') {
+                        $filecontent = <STDIN>;
+                }
+                else
+                {
+                        open (my $FH, "<", $file) or die "Cannot open file $file";
+                        $filecontent = <$FH>;
+                        close $FH;
+                }
+        }
+        if ($intype eq "yaml") {
+                require YAML::Syck;
+                $data = YAML::Syck::Load($filecontent);
+        }
+        elsif ($intype eq "dumper") {
+                eval '$data = my '.$filecontent;
+        }
+        else
+        {
+                die "Unrecognized input type: $intype";
+        }
+        return $data;
+}
+
+sub match {
+        my ($self, $opt, $args, $data, $path) = @_;
+        print "path: $path\n";
+        require Data::Dumper;
+        print "data: ".Data::Dumper::Dumper($data);
+        my @resultlist = dpath($path)->match($data);
+        print Data::Dumper::Dumper(\@resultlist);
+        return \@resultlist;
+}
+
+sub write_out {
+    my ($self, $opt, $args, $resultlist) = @_;
+
+    my $outtype = $opt->{outtype} || 'yaml';
+    print STDERR "outtype: $outtype\n";
+    if ($outtype eq "yaml") {
+            require YAML::Syck;
+            print YAML::Syck::Dump($resultlist);
+    } elsif ($outtype eq "dumper") {
+            require Data::Dumper;
+            print Data::Dumper::Dumper($resultlist);
+    }
+    else
+    {
+            die "Unrecognized output type: $outtype";
+    }
 }
 
 sub run {
-    my ($self, $opt, $args) = @_;
+        my ($self, $opt, $args) = @_;
 
-    my $result = $opt->{blortex} ? "blortex()" : "blort()";
-    recheck($result) if $opt->{recheck};
-    print $result, "\n";
+#        print Data::Dumper::Dumper($opt);
+#        print Data::Dumper::Dumper($args);
+
+        my $path    = $args->[0];
+        my $file    = $args->[1] || '-';
+
+        write_out( $self,
+                   $opt,
+                   $args,
+                   match($self,
+                         $opt,
+                         $args,
+                         read_in( $self,
+                                  $opt,
+                                  $args,
+                                  $file ),
+                         $path ));
 }
 
 # $ yourcmd blort --recheck
